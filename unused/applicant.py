119199bdfb80f3ef6c0321ecbb62466facae477d
Adam Phillips <105430056+TTFrance@users.noreply.github.com>
2022-12-09 12:16:54 +0100

    Delete unused directory

--- unused/applicant.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-class applicant(BaseModel):
-    EXT_SOURCE_2: float # 0 to 1
-    EXT_SOURCE_3: float # 0 to 1
-    PAYMENT_RATE: float # 0 to 1
-    PA_PrLI_DELAY_DAYS_INSTALMENT__max__max: int # -110 to 2400
-    CODE_GENDER_F: int # 0 or 1
--- unused/bankcustomers.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-class bank_customer(BaseModel):
-    param1: float
-    param2: float
-    param3: float
-    param4: float
-
